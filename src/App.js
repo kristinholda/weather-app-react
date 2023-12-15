17fd9a49a91372d5de3ff84e0542bdc58d7ee147
kristinholda <k.holda@yahoo.com>
2023-12-15 14:54:43 -0500

    added open source link to github repo

--- src/App.js
+++ src/App.js
@@ -5,6 +5,17 @@ export default function App() {
     <div className="App">
       <div className="container">
         <h1>React Weather App</h1>
+        <footer>
+          This project was created by Kristin Holda and is{" "}
+          <a
+            href="https://github.com/kristinholda/weather-app-react"
+            target="_blank"
+            rel="noreferrer"
+          >
+            open sourced
+          </a>{" "}
+          on Github.
+        </footer>
       </div>
     </div>
   );
