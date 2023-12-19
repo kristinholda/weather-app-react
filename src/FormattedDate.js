11458a5d569ca2f69a33a6d2f3e7f27796237663
kristinholda <k.holda@yahoo.com>
2023-12-19 13:36:23 -0500

    Updated date formatting

--- src/FormattedDate.js
+++ src/FormattedDate.js
@@ -1,6 +1,7 @@
 import React from "react";
 
 export default function FormattedDate(props) {
+  console.log(props.date);
   let days = [
     "Sunday",
     "Monday",
@@ -10,7 +11,24 @@ export default function FormattedDate(props) {
     "Friday",
     "Saturday",
   ];
+  let months = [
+    "January",
+    "February",
+    "March",
+    "April",
+    "May",
+    "June",
+    "July",
+    "August",
+    "September",
+    "October",
+    "November",
+    "December",
+  ];
   let day = days[props.date.getDay()];
+  let month = months[props.date.getMonth()];
+  let date = props.date.getDate();
+  let year = props.date.getFullYear();
   let hours = props.date.getHours();
   if (hours < 10) {
     hours = `0${hours}`;
@@ -23,7 +41,7 @@ export default function FormattedDate(props) {
 
   return (
     <div>
-      {day} {hours}:{minutes}
+      {day} {month} {date} {year} | {hours}:{minutes}
     </div>
   );
 }
