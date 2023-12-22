b2eb30b40277dba1c53e11211313304f21d90a36
kristinholda <k.holda@yahoo.com>
2023-12-22 13:43:32 -0500

    Added forecast component

--- src/Weather.js
+++ src/Weather.js
@@ -2,6 +2,7 @@ import React, { useState } from "react";
 import axios from "axios";
 import FormattedDate from "./FormattedDate";
 import WeatherInfo from "./WeatherInfo";
+import WeatherForecast from "./WeatherForecast";
 import "./Weather.css";
 
 export default function Weather(props) {
@@ -80,6 +81,7 @@ export default function Weather(props) {
             </div>
 
             <WeatherInfo data={weatherData} />
+            <WeatherForecast />
           </div>
         </div>
       </div>
---
+++ src/WeatherForecast.js
@@ -0,0 +1,23 @@
+import React from "react";
+
+export default function WeatherForecast() {
+  return (
+    <div className="weather-forecast" id="forecast">
+      <div className="row">
+        <div className="col-2 weather-forecast-col">
+          <div className="weather-forecast-date">Tue</div>
+          <img
+            id="weather-icon"
+            src="http://shecodes-assets.s3.amazonaws.com/api/weather/icons/scattered-clouds-day.png"
+            alt=""
+            width="42"
+          />
+          <div className="weather-forecast-temperatures">
+            <span className="weather-forecast-temperature-max"> 37° </span>
+            <span className="weather-forecast-temperature-min"> 32° </span>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
+}
